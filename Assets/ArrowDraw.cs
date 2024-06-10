21cd2dad4e888c65fa5d59a6fd30103b8c70f1fc
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-06-10 10:47:22 +0900

    矢印を描画

--- Assets/ArrowDraw.cs
+++ Assets/ArrowDraw.cs
@@ -1,26 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class ArrowDraw : MonoBehaviour
 {
+    [SerializeField]
+    private Image arrowImage;
+    private Vector3 clickPosition;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private Vector3 clickPosition;
+   
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             clickPosition = Input.mousePosition;
+            arrowImage.gameObject.SetActive(true);
         }
-        if (Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(0)) 
+        { 
+            arrowImage.gameObject.SetActive(false);
+        }
+
+        if (Input.GetMouseButton(0))
         {
             Vector3 dist = clickPosition - Input.mousePosition;
-            Debug.Log(dist);
+            //ベクトルの長さを算出
+            float size = dist.magnitude;
+            //ベクトルから角度(弧度法)を算出
+            float angleRed = Mathf.Atan2(dist.y, dist.x);
+            //矢印の画像をクリックした場所に画像を移動
+            arrowImage.rectTransform.position = clickPosition;
+            //矢印の画像をベクトルから算出した角度を度数法に変換してZ軸回転
+            arrowImage.rectTransform.rotation = Quaternion.Euler(0, 0, angleRed * Mathf.Rad2Deg);
+            //矢印の画像の大きさをドラッグした距離に合わせる
+            arrowImage.rectTransform.sizeDelta = new Vector2 (size, size);
         }
     }
 }
