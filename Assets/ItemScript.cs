c45f29399d48a015f24506a2b9d77a669c8dc8d0
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-06-17 11:13:46 +0900

    Animationの遷移を管理

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ItemScript : MonoBehaviour
 {
+
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +22,8 @@ public class ItemScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Debug.Log("Enter");
-        DestroySelf();
+        //DestroySelf();
+        animator.SetTrigger("Get");
     }
 
     private void OnTriggerStay(Collider other)
