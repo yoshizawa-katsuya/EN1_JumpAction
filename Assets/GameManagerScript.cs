7406b09e181592043e935be83ee38194bf2bb44f
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-07-01 13:05:34 +0900

    修正

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -10,7 +10,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(1280, 720, false);
     }
 
     // Update is called once per frame
